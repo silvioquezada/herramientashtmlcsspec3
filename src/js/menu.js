61d433456bbed79f27fd56b2ed0f15f6d11a6212
“silvioescritorio” <sss.silq@gmail.com>
2023-06-15 06:56:49 -0500

    Ubicar en un solo js módulo para mejorar el hilo del sitio web

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,19 @@
+import Glide from '@glidejs/glide';
+
+  const config = {
+    type: 'carousel',
+    perView: 1,
+    breakpoints:{
+      400:{
+        perView: 1
+      },
+      800:{
+        perView: 1
+      }
+    }
+  };
+  new Glide('.glide', config).mount();
+
 function clickMenu(){
     document.getElementById('menu')
             .classList
